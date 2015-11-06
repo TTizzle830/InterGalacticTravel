e5d6104711b1af28b96e5139ba2a4839253cb877
student3 <student3@jh-mbair-158-13.d214.org>
2015-11-06 14:30:12 -0600

    create a new view controller

---
+++ InterGalacticTravel/ImageViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ImageViewController.swift
+//  InterGalacticTravel
+//
+//  Created by student3 on 11/6/15.
+//  Copyright © 2015 JohnHerseyHighSchool. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+   
+}
